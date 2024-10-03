5c7baaed70f0784c2bea1955887ee279b4133e63
gaibu <1016771049@qq.com>
2024-10-03 10:39:07 +0800

    init

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+import uvicorn
+from fastapi import FastAPI
+
+import main
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host='0.0.0.0', port=8080,  reload=True, log_level='debug')
